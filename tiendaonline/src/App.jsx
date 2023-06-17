727d05125ccc9b8ad7000ad7be8d0925c2f9d1b9
ian881 <ianherrera990@gmail.com>
2023-06-16 21:20:59 -0300

    2tp

--- tiendaonline/src/App.jsx
+++ tiendaonline/src/App.jsx
@@ -3,7 +3,7 @@
 import './App.css'
 import NavScrollExample from './components/NavBar/NavBar';
 import itemlistcontainer from './components/Itemlistcontainer/itemlistcontainer';
-
+import ItemCount from './components/ItemCount/ItemCount';
 
   function App() {
     return (
@@ -12,6 +12,7 @@ import itemlistcontainer from './components/Itemlistcontainer/itemlistcontainer'
           <NavScrollExample></NavScrollExample>
 
           <itemlistcontainer greeting="TiendaOnline">TiendaOnline</itemlistcontainer>
+          <ItemCount initial={1} stock={10} onAdd={(quantity)=> console.log ("cantidad agregada",quantity)}></ItemCount>
          
         </div>
      
